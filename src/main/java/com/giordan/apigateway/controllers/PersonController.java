10517ad3ce5b8d93e905d0659786d89e3859e1c5
Giordan Garcia <giordangarcia12@gmail.com>
2024-04-18 10:54:07 -0300

    Content negotiation via HEADER Parameter for xml

--- src/main/java/com/giordan/apigateway/controllers/PersonController.java
+++ src/main/java/com/giordan/apigateway/controllers/PersonController.java
@@ -15,34 +15,34 @@ public class PersonController {
     private final PersonService personServices;
 
     @GetMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> findPersonById(@PathVariable Long id) {
         return ResponseEntity.ok(personServices.findPersonById(id));
     }
 
     @GetMapping(value = "/all",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> findAll() {
         return ResponseEntity.ok(personServices.findAll());
     }
 
 
      @GetMapping(value = "/v2/all",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> findAllV2() {
         return ResponseEntity.ok(personServices.findAll());
     }
 
     @PostMapping(value = "/create",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> addPersonV2(@RequestBody PersonDto personDto) {
         return ResponseEntity.ok(personServices.addPerson(personDto));
     }
 
     @PostMapping(value = "/v2/create",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> addPerson(@RequestBody PersonDtoV2 personDtoV2) {
         return ResponseEntity.ok(personServices.addPersonV2(personDtoV2));
     }
